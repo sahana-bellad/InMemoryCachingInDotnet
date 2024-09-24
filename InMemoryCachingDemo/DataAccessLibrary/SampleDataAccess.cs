3b6225b684b560b7aff1ef19b78835bfae5a409f
sahana-bella <sahanaballad@gmail.com>
2024-09-24 22:28:09 +0530

    Creation of Asynchronous method to improve performance

--- InMemoryCachingDemo/DataAccessLibrary/SampleDataAccess.cs
+++ InMemoryCachingDemo/DataAccessLibrary/SampleDataAccess.cs
@@ -12,4 +12,15 @@ public class SampleDataAccess
         Thread.Sleep(millisecondsTimeout: 3000);
         return employee;
     }
+
+    public async Task<List<EmployeeModel>> GetEmployeesAsync()
+    {
+        List<EmployeeModel> employee = new();
+        employee.Add(new() { FirstName = "John", LastName = "Doe" });
+        employee.Add(new() { FirstName = "Jane", LastName = "Smith" });
+        employee.Add(new() { FirstName = "Emily", LastName = "White" });
+        employee.Add(new() { FirstName = "David", LastName = "Green" });
+        await Task.Delay(millisecondsDelay: 3000);
+        return employee;
+    }
 }
